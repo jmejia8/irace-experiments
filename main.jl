2b1b1289f59f2f2f4cf71a2f6e64e2dff96e96b8
Jesús Mejía <jesusmejded@gmail.com>
2019-06-05 22:48:08 -0500

    configuing i-race

--- main.jl
+++ main.jl
@@ -3,10 +3,10 @@ module Hello
 using Metaheuristics
 using CEC17
 
-
 function parseArgs(args)
     if length(args)%2 != 0
         @error "No Valid Arguments..."
+        exit(1)
     end
 
     arguments = Dict([ ("--D", 10),  ("--N", 100), ("--K", 7), ("--eta", 2.0), ("--instance", 1)  ])
@@ -14,26 +14,26 @@ function parseArgs(args)
         arguments[args[2i-1]] = parse(Float64, args[2i])
     end
 
-    display(arguments)
+    # display(arguments)
     return Int(arguments["--D"]), Int(arguments["--N"]), Int(arguments["--K"]), Float64(arguments["--eta"]), Int(arguments["--instance"])
 end
 
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
-    # println("hello, world")
 
     D, N, K, η_max, instance = parseArgs(ARGS)
 
-    f(x, fnum=instance) = cec17_test_func(x, fnum)
+    f(x, fnum=instance) = cec17_test_func(x, fnum) - fnum*100.0
 
-    x, fx = eca(f, D; K = K, N = N, η_max=η_max)
+    x, fx = eca(f, D; K = K, N = N, η_max=η_max, showResults=false)
 
 
-    println("best: ", fx)
+    print(fx)
     # display(x)
 
     return 0
 end
 
+# using PackageCompiler
 # build_executable("main.jl", "main")
 
 
